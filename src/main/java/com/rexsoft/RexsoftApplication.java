b3d899afa974f17082ab47eb9852ea51c7d89a5e
Diego Alejandro <diego.fc.1@hotmail.com>
2019-04-17 21:20:34 -0500

    first commit

---
+++ src/main/java/com/rexsoft/RexsoftApplication.java
@@ -0,0 +1,13 @@
+package com.rexsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RexsoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RexsoftApplication.class, args);
+	}
+
+}
